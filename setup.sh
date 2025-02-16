900ff0ec2f29d4a039ab8aab4b1b5f951a1adf98
farhanms123 <farhan.m.sabran@banksinarmas.com>
2025-02-16 21:15:10 +0700

    feat: redo color blend algorithm research

--- setup.sh
+++ setup.sh
@@ -6,7 +6,18 @@ cd ~
 # https://conda-forge.org/download/
 # https://github.com/conda-forge/miniforge?tab=readme-ov-file#install
 # https://stackoverflow.com/questions/60532678/what-is-the-difference-between-miniconda-and-miniforge
-curl -L -o Miniforge3-Linux-x86_64.sh https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
-bash Miniforge3-Linux-x86_64.sh
 
-rm Miniforge3-Linux-x86_64.sh
+# https://github.com/conda-forge/miniforge/releases/tag/24.11.0-0
+# curl -L -o forge3-Linux-x86_64.sh https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
+curl -L -o forge3-Linux-x86_64.sh https://github.com/conda-forge/miniforge/releases/download/24.11.0-0/Miniforge3-Linux-x86_64.sh
+
+# MAMBA IS DEPRECATED; MAMBA already included; PyPy is DEPRECATED
+# https://conda-forge.org/news/2024/07/29/sunsetting-mambaforge/
+# https://conda-forge.org/news/2024/08/14/sunsetting-pypy/
+# curl -L -o forge3-Linux-x86_64.sh https://github.com/conda-forge/miniforge/releases/download/24.11.0-0/Miniforge-pypy3-Linux-x86_64.sh
+# curl -L -o forge3-Linux-x86_64.sh https://github.com/conda-forge/miniforge/releases/download/24.11.0-0/Mambaforge-Linux-x86_64.sh
+# curl -L -o forge3-Linux-x86_64.sh https://github.com/conda-forge/miniforge/releases/download/24.11.0-0/Mambaforge-pypy3-Linux-x86_64.sh
+
+bash forge3-Linux-x86_64.sh
+
+rm forge3-Linux-x86_64.sh
